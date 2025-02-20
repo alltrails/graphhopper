ce67dfe0117d53a500e3b5d02b96aeca2835670a
Joey Stansfield <joey.stansfield@alltrails.com>
2025-02-20 14:16:17 -0700

    DISCO-13738: prod deployment

--- web-bundle/src/main/resources/com/graphhopper/maps/config.js
+++ web-bundle/src/main/resources/com/graphhopper/maps/config.js
@@ -1,5 +1,5 @@
 const config = {
-    routingApi: location.origin + (location.host == 'alpha.mostpaths.com' ? '/api/alltrails/graphhopper-service/' : '/'),
+    routingApi: location.origin + '/api/alltrails/graphhopper-service/',
     geocodingApi: '',
     defaultTiles: 'OpenStreetMap',
     keys: {
